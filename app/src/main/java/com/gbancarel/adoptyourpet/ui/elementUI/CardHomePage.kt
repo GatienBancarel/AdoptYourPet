c85930eb84729365021f376c67bf0f8346de3909
Bancarel Gatien <gatien.bancarel@edu.ece.fr>
2020-08-18 14:13:27 +0200

    suppression commentaires

--- app/src/main/java/com/gbancarel/adoptyourpet/ui/elementUI/CardHomePage.kt
+++ app/src/main/java/com/gbancarel/adoptyourpet/ui/elementUI/CardHomePage.kt
@@ -115,19 +115,12 @@ fun NetworkImageComponentGlide(
     val theImage = image
     val theDrawable = drawable
     if (theImage != null) {
-        // Box is a predefined convenience composable that allows you to apply common draw & layout
-        // logic. In addition we also pass a few modifiers to it.
-
-        // You can think of Modifiers as implementations of the decorators pattern that are
-        // used to modify the composable that its applied to. In this example, we configure the
-        // Box composable to have a max height of 200dp and fill out the entire available
-        // width.
-            // Image is a pre-defined composable that lays out and draws a given [ImageAsset].
-            Image(
-                asset = theImage,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.preferredHeight(64.dp).preferredWidth(64.dp).clip(shape = RoundedCornerShape(32.dp))
-            )
+        Image(
+            asset = theImage,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.preferredHeight(64.dp).preferredWidth(64.dp)
+                .clip(shape = RoundedCornerShape(32.dp))
+        )
     } else if (theDrawable != null) {
         Canvas(modifier = modifier) {
             drawCanvas { canvas, _ ->
